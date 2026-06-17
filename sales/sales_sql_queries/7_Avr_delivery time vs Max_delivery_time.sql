af9d64d72bd313c0530f4ebd7fd60291009b3dd6
mohsinbaouba7-eng <mohsinbaouba7@gmail.com>
2026-06-17 11:25:17 +0000

    update

---
+++ sales/sales_sql_queries/7_Avr_delivery time vs Max_delivery_time.sql
@@ -0,0 +1,12 @@
+SELECT
+   "Item_Type",
+   ROUND(AVG(delivery_time), 2) AS Avg_delivery_time,
+   MAX(delivery_time) AS MaX_delivery_time
+    FROM sales_table
+    GROUP BY "Item_Type"
+    ORDER BY Avg_delivery_time ASC,
+    MaX_delivery_time ;
+
+
+SELECT *
+FROM sales_table
